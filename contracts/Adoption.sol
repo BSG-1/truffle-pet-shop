104b5e4ad23152f8e10613f091817cf31de00010
Ghataorhe <b.ghataorhe@gmail.com>
2018-07-27 14:03:26 -0700

    msg.sender adopter address verification as sender

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,6 +4,10 @@ contract Adoption {
     address[16] public adopters;
 
     function adopt(uint petId) public returns (uint) {
+        //petId has to remain within the array
         require(petId >= 0 && petId <= 15);
+
+        //whoever does the adopting, use their address
+        adopters[petId] = msg.sender;
     }
 }
